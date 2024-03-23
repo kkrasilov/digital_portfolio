f271bf2686fcd1fc16261bc7bbe178b88ca47395
kkrasilov <kkrasilov@gmail.com>
2024-03-23 18:35:01 +0500

    init

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source "https://rubygems.org"
+
+ruby "2.7.5"
+gem "rails", "~> 7.1.3", ">= 7.1.3.2"
+
+gem "pg", "~> 1.1"
+gem "puma", ">= 5.0"
+gem "redis", ">= 4.0.1"
+gem "tzinfo-data", platforms: %i[ mswin mswin64 mingw x64_mingw jruby ]
+gem "bootsnap", require: false
+gem 'dotenv-rails'
+gem "sassc-rails"
+gem "vite_rails"
+
+group :development, :test do
+  gem 'pry-rails'
+end
---
+++ config/deploy.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'
+
+set :application_name, 'digital_portfolio'
+set :domain, '37.230.113.230'
+set :user, 'deploy'
+set :deploy_to, '/var/www/digital'
+set :repository, 'git@github.com:kkrasilov/digital_portfolio.git'
+set :branch, 'master'
+set :bundle_without, %w{development test}.join(':')
+set :shared_files, fetch(:shared_files, []).push(
+  'config/database.yml',
+  'config/credentials.yml.enc',
+  'config/master.key'
+)
+
+task :remote_environment do
+  invoke :'rbenv:load'
+  invoke :'nvm:load'
+end
+
+task :setup do
+  invoke :'nvm'
+end
+
+desc 'Deploys the current version to the server.'
+task :deploy do
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle'
+    invoke :'nvm:install'
+    invoke :'rails:db_migrate'
+    command %(bin/vite clobber)
+    command %(bin/vite build)
+    invoke :'deploy:cleanup'
+
+    on :launch do
+      in_path(fetch(:current_path)) do
+        command %(mkdir -p tmp/)
+        command %(touch tmp/restart.txt)
+      end
+    end
+  end
+end
+
+task :nvm do
+  command 'echo "-----> Installing Node Version Manager"'
+  command 'curl -s https://raw.github.com/creationix/nvm/master/install.sh | sh'
+  invoke :'nvm:load'
+end
+
+namespace :nvm do
+  task :load do
+    command 'echo "-----> Loading nvm"'
+    command %{
+      source ~/.nvm/nvm.sh
+    }
+    command 'echo "-----> Now using nvm v.`nvm --version`"'
+  end
+
+  task :install do
+    command %{
+      echo "-----> Install node v.#{'18.14.0'}"
+      nvm install #{'18.14.0'}
+      ln -s ${NVM_BIN} ./.bin
+    }
+  end
+end
