31336fe70e43a801c032f0862808ee3731eaba96
kkrasilov <kkrasilov@gmail.com>
2024-04-20 15:00:09 +0500

    add users profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :login, presence: true
   validates :email, presence: true
+
+  has_one_attached :avatar
+
+  def short_name
+    "#{surname} #{first_name}"
+  end
 end
